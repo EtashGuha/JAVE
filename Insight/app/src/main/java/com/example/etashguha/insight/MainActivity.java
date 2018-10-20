4ad4704fa1983eee5b55c1e7a682001f2a4a1824
Etash Guha <etashguha@lawn-128-61-115-75.lawn.gatech.edu>
2018-10-20 01:04:10 -0400

    basic pdfReader

--- Insight/app/src/main/java/com/example/etashguha/insight/MainActivity.java
+++ Insight/app/src/main/java/com/example/etashguha/insight/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.etashguha.insight;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.barteksc.pdfviewer.PDFView;
+
 public class MainActivity extends AppCompatActivity {
 
+    PDFView pdfView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pdfView = findViewById(R.id.pdfView);
+        pdfView.fromAsset("19966572_eTranscript.pdf").load();
     }
 }
